6d9a1552aedd9bcb4efc9e8436d36e8903a78697
57070129 <salinee.4596@gmail.com>
2015-12-05 16:06:25 +0700

    เพิ่มกราฟรับโลหิตของเดือนพฤษภาคม

---
+++ may_get.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+# The slices will be ordered and plotted counter-clockwise.
+def may_get():
+    labels = 'FirstDonation', 'GetPlace', 'GetMove', 'GetFebruary'
+    sizes = [6571, 24377, 25692, 13507]
+    colors = ['mediumorchid', 'teal', 'gold', 'indianred']
+    explode = (0, 0, 0.1, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+    plt.pie(sizes, explode=explode, labels=labels, colors=colors,
+        autopct='%1.1f%%', shadow=True, startangle=90)
+    # Set aspect ratio to be equal so that pie is drawn as a circle.
+    plt.axis('equal')
+    plt.show()
+may_get()
